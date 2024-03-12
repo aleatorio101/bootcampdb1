0d45a70f373b871800c98d3308ed38716bccf72f
Bownie <lukasdng@gmail.com>
2024-03-11 21:52:58 -0300

    modified:   verificardorDePares.js

--- verificardorDePares.js
+++ verificardorDePares.js
@@ -2,7 +2,6 @@ const encontrarPares = (array) => {
     return array.filter(num => num % 2 === 0);
 }
 
-// Exemplo de uso
 const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const pares = encontrarPares(numeros);
 console.log("Números pares:", pares);
