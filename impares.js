661200656b75cb57c806a8de0670f2f20419c0eb
aleatorio101 <149822932+aleatorio101@users.noreply.github.com>
2024-03-11 22:24:14 -0300

    Update impares.js

--- impares.js
+++ impares.js
@@ -2,6 +2,6 @@ const encontrarImpares = (array) => {
     return array.filter(num => num % 2 !== 0);
 }
 
-const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 20, 17];
 const impares = encontrarImpares(numeros);
-console.log("Números impares:", impares);
+console.log("os números impares são:", impares);
